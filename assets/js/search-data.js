1cebe9b067d6e25cae1cf66eb4c98a91e11ce8b0
Vsaggiomo <vsaggiomo@gmail.com>
2026-01-04 21:50:33 +0000

    Deploying to gh-pages from @ Vsaggiomo/vsaggiomo.github.io@e390a865de4187596b91987e2b986f69f9b5ed54 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,8 +23,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
+        },{id: "nav-research",
+          title: "research",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
