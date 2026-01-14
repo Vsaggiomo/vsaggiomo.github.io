0522a0d7e0dacf1fa50164217280af34f7fffd06
Vsaggiomo <vsaggiomo@gmail.com>
2026-01-14 20:44:17 +0000

    Deploying to gh-pages from @ Vsaggiomo/vsaggiomo.github.io@fb4820bcf31e8452a631924edf26420e381ac6b2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -428,8 +428,8 @@ ninja.data = [{
           description: "democratizing microfluidics, one chip at the time",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
+            },},{id: "projects-sense",
+          title: 'Sense',
           description: "Sensing From the Lab to the Field",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
